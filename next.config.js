851f3f8a51943a115a512a19ad6399da06b5ea3f
CORASYSTEMS\ewalsh <ewalsh@corasystems.com>
2022-05-11 12:14:31 +0100

    adding ignore linting setting

--- next.config.js
+++ next.config.js
@@ -4,5 +4,10 @@ module.exports = {
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'en',
-  },
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
